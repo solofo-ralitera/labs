eb9d320255f6d4bae31816f897d587770f89e43d
Solofo <solofo.ralitera@gmail.com>
2023-06-24 14:24:15 +0300

    s4

--- ts/main.js
+++ ts/main.js
@@ -1,13 +1,74 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Main = void 0;
-var Main = /** @class */ (function () {
-    function Main() {
-        console.log('Construct');
+var StdOutput = /** @class */ (function () {
+    function StdOutput() {
     }
-    Main.prototype.log = function () {
-        return 'azeaze';
+    StdOutput.prototype.write = function (text) {
+        console.log('Stdout write', text);
     };
-    return Main;
+    return StdOutput;
 }());
-exports.Main = Main;
+var FileOutput = /** @class */ (function () {
+    function FileOutput() {
+    }
+    FileOutput.prototype.write = function (text) {
+        console.log('File write', text);
+    };
+    FileOutput.prototype.delete = function (text) {
+        console.log('text deleted', text);
+    };
+    return FileOutput;
+}());
+var CdOutput = /** @class */ (function () {
+    function CdOutput() {
+    }
+    CdOutput.prototype.write = function (text) {
+        console.log('Cd burn', text);
+    };
+    return CdOutput;
+}());
+var TableauOutput = /** @class */ (function () {
+    function TableauOutput() {
+    }
+    TableauOutput.prototype.write = function (text) {
+        console.log('Tableau', text);
+    };
+    TableauOutput.prototype.delete = function (text) {
+        console.log('delete text', text);
+    };
+    return TableauOutput;
+}());
+var BddOutput = /** @class */ (function () {
+    function BddOutput() {
+    }
+    BddOutput.prototype.write = function (text) {
+        console.log('Bdd', text);
+    };
+    BddOutput.prototype.delete = function (text) {
+        console.log('delete text', text);
+    };
+    return BddOutput;
+}());
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    Person.prototype.ecrire = function (text, support) {
+        support.write(text);
+    };
+    Person.prototype.delete = function (text, support) {
+        support.delete(text);
+    };
+    return Person;
+}());
+var stdOut = new StdOutput();
+var fileOut = new FileOutput();
+var cdOut = new CdOutput();
+var tableauOut = new TableauOutput();
+var bddOut = new BddOutput();
+var person = new Person();
+person.ecrire('hello', stdOut);
+person.ecrire('hello', fileOut);
+person.ecrire('hello', cdOut);
+person.ecrire('hello', tableauOut);
+person.ecrire('hello', bddOut);
+person.delete('hello', fileOut);
+person.delete('hello', tableauOut);
+person.delete('hello', bddOut);
--- ts/main.ts
+++ ts/main.ts
@@ -1,14 +1,76 @@
-export class Main implements ITest {
-  constructor() {
-    console.log('Construct');
+
+interface Deletable {
+  delete(text: string): void;
+}
+
+interface Writable {
+  write(text: string): void;
+}
+
+class StdOutput implements Writable {
+  public write(text: string): void {
+    console.log('Stdout write', text);
   }
 
-  log(): string {
-    return 'azeaze';
+}
+
+class FileOutput implements Writable, Deletable {
+  public write(text: string): void {
+    console.log('File write', text);
+  }
+  public delete(text: string): void {
+    console.log('text deleted', text);
+  }  
+}
+
+class CdOutput implements Writable {
+  public write(text: string) {
+    console.log('Cd burn', text);
   }
 }
 
-
-export interface ITest {
-  log(): string ;
+class TableauOutput implements Writable, Deletable {
+  public write(text: string) {
+    console.log('Tableau', text);
+  }
+  public delete(text: string): void {
+    console.log('delete text', text);
+  }  
 }
+
+class BddOutput implements Writable, Deletable {
+  public write(text: string) {
+    console.log('Bdd', text);
+  }
+  public delete(text: string): void {
+    console.log('delete text', text);
+  }  
+}
+
+class Person {
+  public ecrire(text: string, support: Writable): void {
+    support.write(text);
+  }
+
+  public delete(text: string, support: Deletable): void {
+    support.delete(text);
+  }
+}
+
+const stdOut = new StdOutput();
+const fileOut = new FileOutput();
+const cdOut = new CdOutput();
+const tableauOut = new TableauOutput();
+const bddOut = new BddOutput();
+
+const person = new Person();
+person.ecrire('hello', stdOut);
+person.ecrire('hello', fileOut);
+person.ecrire('hello', cdOut);
+person.ecrire('hello', tableauOut);
+person.ecrire('hello', bddOut);
+
+
+person.delete('hello', fileOut);
+person.delete('hello', tableauOut);
+person.delete('hello', bddOut);
