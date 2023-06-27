070d8840d1afd2936d9cbc4624d642fb46079001
Solofo <solofo.ralitera@gmail.com>
2023-06-27 13:07:37 +0300

    etape 2

--- ts/main.js
+++ ts/main.js
@@ -1,3 +1,10 @@
+/**
+ * Exo1: un personne peut ecrire sur un support (standard output, fichier, bdd, tableau, cahier...)
+ *
+ * Etape 1:
+ *  - 2 classes:  Person et StdOutput
+ * Evolution: rajouter un nouveau support pour ecrire
+ */
 var StdOutput = /** @class */ (function () {
     function StdOutput() {
     }
@@ -6,6 +13,14 @@ var StdOutput = /** @class */ (function () {
     };
     return StdOutput;
 }());
+var Fichier = /** @class */ (function () {
+    function Fichier() {
+    }
+    Fichier.prototype.ecrire = function (text) {
+        console.log('Fichier ecrire: ', text);
+    };
+    return Fichier;
+}());
 var Person = /** @class */ (function () {
     function Person() {
     }
@@ -13,7 +28,12 @@ var Person = /** @class */ (function () {
         var output = new StdOutput();
         output.ecrire(text);
     };
+    Person.prototype.ecrireFichier = function (text) {
+        var output = new Fichier();
+        output.ecrire(text);
+    };
     return Person;
 }());
 var moi = new Person();
 moi.ecrireStdOutput('Hello');
+moi.ecrireFichier('Hello');
--- ts/main.ts
+++ ts/main.ts
@@ -1,9 +1,13 @@
 /**
  * Exo1: un personne peut ecrire sur un support (standard output, fichier, bdd, tableau, cahier...)
  * 
- * Etape 1: 
- *  - 2 classes:  Person et StdOutput
- * Evolution: rajouter un nouveau support pour ecrire
+ * Etape 2: 
+ *  une classs par type de support
+ * Contrainte: 
+ *  - si nouveau support, il faut rajouter la méthode d'ecriture correspondants à ce support 
+ *  - on a une dépendance de chaque support dans Person (ligne 27 et 32) + new à chaque ecriture
+ * 
+ * Evolution: traiter cette dépendance et minimiser les modifications à faire à chaque rajout de support
  */
 
 class StdOutput {
@@ -12,12 +16,25 @@ class StdOutput {
   }
 }
 
+class Fichier {
+  public ecrire(text: string): void {
+    console.log('Fichier ecrire: ', text);
+  }
+}
+
 class Person {
   public ecrireStdOutput(text: string) {
     const output = new StdOutput();
     output.ecrire(text);
   }
+
+  public ecrireFichier(text: string) {
+    const output = new Fichier();
+    output.ecrire(text);
+  }
+
 }
 
 const moi = new Person();
 moi.ecrireStdOutput('Hello');
+moi.ecrireFichier('Hello');
