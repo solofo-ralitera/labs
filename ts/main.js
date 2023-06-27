d965b7710f4647ab0f9defeca5a65a29759f7510
Solofo <solofo.ralitera@gmail.com>
2023-06-27 13:03:20 +0300

    etape 1

--- ts/main.js
+++ ts/main.js
@@ -1,74 +1,19 @@
 var StdOutput = /** @class */ (function () {
     function StdOutput() {
     }
-    StdOutput.prototype.write = function (text) {
-        console.log('Stdout write', text);
+    StdOutput.prototype.ecrire = function (text) {
+        console.log('Standard output: ', text);
     };
     return StdOutput;
-}());
-var FileOutput = /** @class */ (function () {
-    function FileOutput() {
-    }
-    FileOutput.prototype.write = function (text) {
-        console.log('File write', text);
-    };
-    FileOutput.prototype.delete = function (text) {
-        console.log('text deleted', text);
-    };
-    return FileOutput;
-}());
-var CdOutput = /** @class */ (function () {
-    function CdOutput() {
-    }
-    CdOutput.prototype.write = function (text) {
-        console.log('Cd burn', text);
-    };
-    return CdOutput;
-}());
-var TableauOutput = /** @class */ (function () {
-    function TableauOutput() {
-    }
-    TableauOutput.prototype.write = function (text) {
-        console.log('Tableau', text);
-    };
-    TableauOutput.prototype.delete = function (text) {
-        console.log('delete text', text);
-    };
-    return TableauOutput;
-}());
-var BddOutput = /** @class */ (function () {
-    function BddOutput() {
-    }
-    BddOutput.prototype.write = function (text) {
-        console.log('Bdd', text);
-    };
-    BddOutput.prototype.delete = function (text) {
-        console.log('delete text', text);
-    };
-    return BddOutput;
 }());
 var Person = /** @class */ (function () {
     function Person() {
     }
-    Person.prototype.ecrire = function (text, support) {
-        support.write(text);
-    };
-    Person.prototype.delete = function (text, support) {
-        support.delete(text);
+    Person.prototype.ecrireStdOutput = function (text) {
+        var output = new StdOutput();
+        output.ecrire(text);
     };
     return Person;
 }());
-var stdOut = new StdOutput();
-var fileOut = new FileOutput();
-var cdOut = new CdOutput();
-var tableauOut = new TableauOutput();
-var bddOut = new BddOutput();
-var person = new Person();
-person.ecrire('hello', stdOut);
-person.ecrire('hello', fileOut);
-person.ecrire('hello', cdOut);
-person.ecrire('hello', tableauOut);
-person.ecrire('hello', bddOut);
-person.delete('hello', fileOut);
-person.delete('hello', tableauOut);
-person.delete('hello', bddOut);
+var moi = new Person();
+moi.ecrireStdOutput('Hello');
--- ts/main.ts
+++ ts/main.ts
@@ -1,76 +1,23 @@
+/**
+ * Exo1: un personne peut ecrire sur un support (standard output, fichier, bdd, tableau, cahier...)
+ * 
+ * Etape 1: 
+ *  - 2 classes:  Person et StdOutput
+ * Evolution: rajouter un nouveau support pour ecrire
+ */
 
-interface Deletable {
-  delete(text: string): void;
-}
-
-interface Writable {
-  write(text: string): void;
-}
-
-class StdOutput implements Writable {
-  public write(text: string): void {
-    console.log('Stdout write', text);
+class StdOutput {
+  public ecrire(text: string): void {
+    console.log('Standard output: ', text);
   }
-
-}
-
-class FileOutput implements Writable, Deletable {
-  public write(text: string): void {
-    console.log('File write', text);
-  }
-  public delete(text: string): void {
-    console.log('text deleted', text);
-  }  
-}
-
-class CdOutput implements Writable {
-  public write(text: string) {
-    console.log('Cd burn', text);
-  }
-}
-
-class TableauOutput implements Writable, Deletable {
-  public write(text: string) {
-    console.log('Tableau', text);
-  }
-  public delete(text: string): void {
-    console.log('delete text', text);
-  }  
-}
-
-class BddOutput implements Writable, Deletable {
-  public write(text: string) {
-    console.log('Bdd', text);
-  }
-  public delete(text: string): void {
-    console.log('delete text', text);
-  }  
 }
 
 class Person {
-  public ecrire(text: string, support: Writable): void {
-    support.write(text);
-  }
-
-  public delete(text: string, support: Deletable): void {
-    support.delete(text);
+  public ecrireStdOutput(text: string) {
+    const output = new StdOutput();
+    output.ecrire(text);
   }
 }
 
-const stdOut = new StdOutput();
-const fileOut = new FileOutput();
-const cdOut = new CdOutput();
-const tableauOut = new TableauOutput();
-const bddOut = new BddOutput();
-
-const person = new Person();
-person.ecrire('hello', stdOut);
-person.ecrire('hello', fileOut);
-person.ecrire('hello', cdOut);
-person.ecrire('hello', tableauOut);
-person.ecrire('hello', bddOut);
-
-
-person.delete('hello', fileOut);
-person.delete('hello', tableauOut);
-person.delete('hello', bddOut);
+const moi = new Person();
+moi.ecrireStdOutput('Hello');
